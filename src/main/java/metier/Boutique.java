77f2795b43416f1c1c3d3d96bee7e5d1b4fa62db
Sammydoo <huot.clement86@gmail.com>
2022-11-24 13:13:02 +0100

    ajout client

---
+++ src/main/java/metier/Boutique.java
@@ -0,0 +1,43 @@
+package metier;
+
+public class Boutique {
+	private String nom;
+	private Adresse adresse;
+	
+	
+	public Boutique(String nom, Adresse adresse) {
+
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Boutique [nom=" + nom + "]";
+	}
+	
+	
+	
+
+}
