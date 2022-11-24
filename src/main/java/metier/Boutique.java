79dc8050ca073405e326f69b3907d2da5e803cc0
JoannaNjeim <joanna.njeim@hotmail.com>
2022-11-24 14:26:17 +0100

    Classe adresse ajoutée

--- src/main/java/metier/Boutique.java
+++ src/main/java/metier/Boutique.java
@@ -3,38 +3,27 @@ package metier;
 public class Boutique {
 	private String nom;
 	private Adresse adresse;
-	
-	
-	public Boutique(String nom, Adresse adresse) {
-
-		this.nom = nom;
-		this.adresse = adresse;
-	}
-
-
 	public String getNom() {
 		return nom;
 	}
-
-
+	
+	public Boutique(String nom, Adresse adresse) {
+		this.nom = nom;
+        this.adresse=adresse;
+	}
 	public void setNom(String nom) {
+	
 		this.nom = nom;
 	}
-
-
-	public Adresse getAdresse() {
+	public Adresse getadresse() {
 		return adresse;
 	}
-
-
-	public void setAdresse(Adresse adresse) {
-		this.adresse = adresse;
+	public void setAdresse(String adresse) {
+		adresse = adresse;
 	}
-
-
 	@Override
 	public String toString() {
-		return "Boutique [nom=" + nom + "]";
+		return "Boutique [nom=" + nom + ", Adresse=" + adresse + "]";
 	}
 	
 	
