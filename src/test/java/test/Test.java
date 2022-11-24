a681d60282ce35f8593e22e1c63aaa67b82be1b5
Sammydoo <huot.clement86@gmail.com>
2022-11-24 15:10:23 +0100

    ajout liste jeux

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -2,14 +2,21 @@ package test;
 
 
 import java.time.LocalDate;
-import metier.Client;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import metier.Adresse;
 import metier.Boutique;
+import metier.Client;
 import metier.Console;
 import metier.Jeu;
 public class Test {
 
 	public static void main(String[] args) {
+		
+		
+		
 		Client client2= new Client("Ganondorf","Dragmire");
 		Client client1= new Client("Samus","Aranne");
 		Console consol1 = new Console("consol1", 12, LocalDate.parse("2000-10-31"));
@@ -22,6 +29,12 @@ public class Test {
 		Jeu jeu3 = new Jeu("titre3",consol1, boutique);
 		Jeu jeu4 = new Jeu("titre4",consol1,boutique);
 		Jeu jeu5 = new Jeu("titre5",consol1,boutique);
+		
+		List<Jeu> jeux=new ArrayList();
+		
+		Collections.addAll(jeux,jeu1,jeu2,jeu3,jeu4);
+		
+		client1.setJeux(jeux);
 
 
 	}
